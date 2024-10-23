3b0530ad34640a4a2bfac6ac74657a390384aba6
Zhannur Kainedin <230030@astanait.edu.kz>
2024-10-23 16:22:46 +0500

    made some changes

--- src/Observer/Main.java
+++ src/Observer/Main.java
@@ -1,14 +1,24 @@
 package Observer;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         NewsAgency agency = new NewsAgency();
+
         NewsChannel channel1 = new NewsChannel("Channel 1");
         NewsChannel channel2 = new NewsChannel("Channel 2");
 
         agency.addObserver(channel1);
         agency.addObserver(channel2);
 
-        agency.setLatestNews("Breaking News: New Observer Pattern Implemented!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the latest news: ");
+        String news = scanner.nextLine();
+
+        agency.setLatestNews(news);
+
+        scanner.close();
     }
 }
